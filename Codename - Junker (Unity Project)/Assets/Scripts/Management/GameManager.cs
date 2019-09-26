aecfa7d07117dff2f6c5c4647c422b5548b5c3bc
hj <h[afiwhd>
2019-09-26 15:07:53 +0100

    S1C1 - Movement system
    The movement of the player placeholder is now implemented using physics. The player moves using pitch, yaw and roll alongside an acceleration and deceleration mechanic. Pitch, yaw and roll are done using torque and the acceleration is done using an add force.

---
+++ Codename - Junker (Unity Project)/Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager s_instance;
+
+    [SerializeField, Range(0,1), Tooltip("The speed of objects in the game on a scale of 0-1")]
+    private float m_gameSpeed;
+
+    public static GameManager Instance { get => s_instance; set => s_instance = value; }
+    public float GameSpeed { get => m_gameSpeed; set => m_gameSpeed = value; }
+
+    private void Awake()
+    {
+        if(s_instance == null)
+        {
+            s_instance = this;
+        }
+        else
+        {
+            Destroy(s_instance.gameObject);
+            s_instance = this;
+        }
+    }
+}
