b1758e98f338c74f5834bd2dfa71ab1180768053
Jack <esf>
2019-09-30 13:38:21 +0100

    S1C4 - Crosshair UI
    A simple script that draws a square in the middle screen has been added.

---
+++ Codename - Junker (Unity Project)/Assets/Scripts/UI/Player/Crosshair.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public Texture2D crosshair;
+    private Rect position;
+
+    [SerializeField, Tooltip("Changes the height of the crosshair on screen")]
+    private float m_heightMod;
+
+    private void Start()
+    {
+        crosshair = Texture2D.whiteTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = new Rect((Screen.width - crosshair.width) / 2, (Screen.height - crosshair.height + m_heightMod) / 2, crosshair.width, crosshair.height);
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(position, crosshair);
+    }
+}
