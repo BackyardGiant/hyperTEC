846453db40bb4e55be70236e190139e30abca086
Aden <adenwebb21@gmail.com>
2019-10-01 13:13:57 +0100

    S1C2 - Built Course
    I have built a course for the usability testing in the flight testing scene.

--- Codename - Junker (Unity Project)/Assets/Scripts/Usability Testing/IncrementFlightChallenge.cs
+++ Codename - Junker (Unity Project)/Assets/Scripts/Usability Testing/IncrementFlightChallenge.cs
@@ -7,16 +7,16 @@ public class IncrementFlightChallenge : MonoBehaviour
     [SerializeField]
     private GameObject pointerArrow, RingCollider;
 
-
-
-    
-
-
     private float CircleRadius;
     public void RecordPlayerScore(Transform player)
     {
         float Distance = Vector3.Distance(transform.position, player.transform.position);
         int Score = Mathf.RoundToInt(100- (Distance/StartFlightTest.Instance.RingRadius * 100));
+
+        if(Score < 0)
+        {
+            Score = Mathf.Abs(Score);
+        }
 
         Debug.Log("Player Scored " + Score);
 
