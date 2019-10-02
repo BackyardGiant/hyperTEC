3e41ba96d80cb8e4bbdf08432aad23ef03cca1a9
Jack <jackbrewer@students.plymouth.ac.uk>
2019-10-02 10:22:21 +0100

    HOTFIX - Fixing Merging Issues
    There was an issue when merging two branches into staging, this fixed it

--- Codename - Junker (Unity Project)/Assets/Scripts/Movement/CameraMovement.cs
+++ Codename - Junker (Unity Project)/Assets/Scripts/Movement/CameraMovement.cs
@@ -12,7 +12,7 @@ public class CameraMovement : MonoBehaviour
     [SerializeField, Header("Camera Position"), Tooltip("Select where the camera will follow the player from")]
     private CameraPositions m_cameraPosition;
 
-    [SerializeField, Header("Player"),Tooltip("The player object should go here")]
+    [SerializeField, Header("Player"), Tooltip("The player object should go here")]
     private Transform m_trPlayerTransform;
     [SerializeField, Tooltip("The offset of the angels")]
     private Vector3 m_offSet;
@@ -24,6 +24,16 @@ public class CameraMovement : MonoBehaviour
     [SerializeField, Tooltip("The smoothing of the camera follow")]
     private float m_smoothTime = 0.3F;
     private Vector3 m_velocity = Vector3.zero;
+
+    [SerializeField, Header("Cameras"), Tooltip("This camera")]
+    private Camera m_followCamera;
+    [SerializeField, Tooltip("The look around camera")]
+    private Transform m_orbitalCameraRig;
+    [SerializeField, Tooltip("The ship object")]
+    private GameObject m_focusObject;
+
+    [SerializeField, Header("Crosshair")]
+    private Crosshair m_crosshair;
 
     void Update()
     {
@@ -37,24 +47,49 @@ public class CameraMovement : MonoBehaviour
         //        break;
         //}
 
-        // Define a target position above and behind the target transform
-        Vector3 targetPosition = m_trPlayerTransform.TransformPoint(m_cameraFollowPos);
+        if (Mathf.Abs(Input.GetAxis("LookX")) > 0 || Mathf.Abs(Input.GetAxis("LookY")) > 0)
+        {
+            m_crosshair.enabled = false;
+            FollowCameraMovement(m_orbitalCameraRig.position, true);
+        }
+        else if (Vector3.Distance(m_trPlayerTransform.TransformPoint(m_cameraFollowPos), transform.position) > 25f)
+        {
+            m_crosshair.enabled = false;
+            m_orbitalCameraRig.GetComponent<OrbitalCamera>().CurrentX = 0;
+            m_orbitalCameraRig.GetComponent<OrbitalCamera>().CurrentY = 0;
+            FollowCameraMovement(m_trPlayerTransform.TransformPoint(m_cameraFollowPos), true);
+        }
+        else
+        {
+            m_crosshair.enabled = true;
+            m_orbitalCameraRig.GetComponent<OrbitalCamera>().CurrentX = 0;
+            m_orbitalCameraRig.GetComponent<OrbitalCamera>().CurrentY = 0;
+            FollowCameraMovement(m_trPlayerTransform.TransformPoint(m_cameraFollowPos), false);
+        }
+    }
 
-        float targetDistance = Vector3.Distance(transform.position, targetPosition);
+    private void FollowCameraMovement(Vector3 _targetPosition, bool _lookAtPlayer)
+    {
 
-        // Smoothly move the camera towards that target position
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref m_velocity, Mathf.Clamp((m_smoothTime - (targetDistance * 0.005f)) / (GameManager.Instance.GameSpeed), 0.07f, 0.12f));
 
         //transform.LookAt(m_trPlayerTarget);
 
+        if (_lookAtPlayer)
+        {
+            transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * 100);
 
-        transform.localEulerAngles = m_trPlayerTransform.localEulerAngles + m_offSet;
+            transform.eulerAngles = m_orbitalCameraRig.eulerAngles;
+        }
+        else
+        {
+            float targetDistance = Vector3.Distance(transform.position, _targetPosition);
+
+            // Smoothly move the camera towards that target position
+            transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref m_velocity, Mathf.Clamp((m_smoothTime) / (GameManager.Instance.GameSpeed), 0.07f, 0.12f));
+
+            transform.localEulerAngles = m_trPlayerTransform.localEulerAngles + m_offSet;
+        }
         //Debug.Log(m_trPlayerTransform.localEulerAngles.z);
         //Debug.Log(transform.localEulerAngles.z);
-    }
-
-    private void NormalCamera()
-    {
-        
     }
 }
---
+++ Codename - Junker (Unity Project)/Assets/Scripts/Movement/OrbitalCamera.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalCamera : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = -50.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+    private const float X_ANGLE_MIN = -155.0f;
+    private const float X_ANGLE_MAX = 155.0f;
+    public Transform lookAt;
+    public GameObject target;
+
+    private float distance = 1.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    public float turningSpeed = 60;
+
+    public float rotateSpeed = 5;
+    Vector3 offset;
+
+    public float CameraDistance;
+    public float CameraAngle;
+
+    public float CurrentX { get => currentX; set => currentX = value; }
+    public float CurrentY { get => currentY; set => currentY = value; }
+
+    private void Start()
+    {
+        offset = target.transform.localPosition - transform.localPosition;
+    }
+
+    private void Update()
+    {
+        CurrentY = Input.GetAxis("LookX") * turningSpeed * Y_ANGLE_MAX;
+        CurrentX = Input.GetAxis("LookY") * turningSpeed * X_ANGLE_MAX;
+
+        CurrentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        CurrentX = Mathf.Clamp(currentX, X_ANGLE_MIN, X_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+
+        //if (Input.GetAxis("Vertical") == 0)
+        //{
+        //    Vector3 dir = new Vector3(0, 0, distance);
+        //    Quaternion rotation = Quaternion.Euler(-1 * currentY, currentX, 0);
+        //    camTransform.position = Vector3.Lerp(transform.position, lookAt.position + rotation * dir * 1.6f, Time.deltaTime * 3);
+        //    camTransform.LookAt(lookAt.position);
+        //}
+
+        //else
+        //{
+        Quaternion rotation = Quaternion.Euler(-1 * currentY, currentX, 0);
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, target.transform.localPosition - (rotation * offset * CameraDistance), Time.deltaTime * 4);
+
+        transform.LookAt(target.transform.position, target.transform.up);
+        //}
+    }
+}
