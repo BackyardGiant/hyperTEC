45edf68e7f30b521cedc0a77bbf12b55cf37e330
Jack Brewer <h[afiwhd>
2019-10-03 16:03:35 +0100

    HOTFIX - Orbital camera fix
    The orbital camera logic was lost due to a merge, this commit fixes that loss of code.

--- Codename - Junker (Unity Project)/Assets/Scripts/Movement/CameraMovement.cs
+++ Codename - Junker (Unity Project)/Assets/Scripts/Movement/CameraMovement.cs
@@ -12,7 +12,7 @@ public class CameraMovement : MonoBehaviour
     [SerializeField, Header("Camera Position"), Tooltip("Select where the camera will follow the player from")]
     private CameraPositions m_cameraPosition;
 
-    [SerializeField, Header("Player"),Tooltip("The player object should go here")]
+    [SerializeField, Header("Player"), Tooltip("The player object should go here")]
     private Transform m_trPlayerTransform;
     [SerializeField, Tooltip("The offset of the angels")]
     private Vector3 m_offSet;
@@ -25,18 +25,55 @@ public class CameraMovement : MonoBehaviour
     private float m_smoothTime = 0.3F;
     private Vector3 m_velocity = Vector3.zero;
 
+    [SerializeField, Header("Cameras"), Tooltip("This camera")]
+    private Camera m_followCamera;
+    [SerializeField, Tooltip("The look around camera")]
+    private Transform m_orbitalCameraRig;
+    [SerializeField, Tooltip("The ship object")]
+    private GameObject m_focusObject;
+
+    [SerializeField, Header("Crosshair")]
+    private Crosshair m_crosshair;
+
     void Update()
     {
-        // Define a target position above and behind the target transform
-        Vector3 targetPosition = m_trPlayerTransform.TransformPoint(m_cameraFollowPos);
+        if (Mathf.Abs(Input.GetAxis("LookX")) > 0 || Mathf.Abs(Input.GetAxis("LookY")) > 0)
+        {
+            m_crosshair.enabled = false;
+            FollowCameraMovement(m_orbitalCameraRig.position, true);
+        }
+        else if (Vector3.Distance(m_trPlayerTransform.TransformPoint(m_cameraFollowPos), transform.position) > 25f)
+        {
+            m_crosshair.enabled = false;
+            m_orbitalCameraRig.GetComponent<OrbitalCamera>().CurrentX = 0;
+            m_orbitalCameraRig.GetComponent<OrbitalCamera>().CurrentY = 0;
+            FollowCameraMovement(m_trPlayerTransform.TransformPoint(m_cameraFollowPos), true);
+        }
+        else
+        {
+            m_crosshair.enabled = true;
+            m_orbitalCameraRig.GetComponent<OrbitalCamera>().CurrentX = 0;
+            m_orbitalCameraRig.GetComponent<OrbitalCamera>().CurrentY = 0;
+            FollowCameraMovement(m_trPlayerTransform.TransformPoint(m_cameraFollowPos), false);
+        }
+    }
 
-        float targetDistance = Vector3.Distance(transform.position, targetPosition);
+    private void FollowCameraMovement(Vector3 _targetPosition, bool _lookAtPlayer)
+    {
+        if (_lookAtPlayer)
+        {
+            transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * 100);
 
-        // Smoothly move the camera towards that target position
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref m_velocity, Mathf.Clamp((m_smoothTime - (targetDistance * 0.005f)) / (GameManager.Instance.GameSpeed), 0.07f, 0.12f));
+            transform.eulerAngles = m_orbitalCameraRig.eulerAngles;
+        }
+        else
+        {
+            float targetDistance = Vector3.Distance(transform.position, _targetPosition);
 
-        //transform.eulerAngles = m_trPlayerTransform.eulerAngles + m_offSet;
+            // Smoothly move the camera towards that target position
+            transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref m_velocity, Mathf.Clamp((m_smoothTime) / (GameManager.Instance.GameSpeed), 0.07f, 0.12f));
 
-        transform.eulerAngles = m_trPlayerTransform.eulerAngles + m_trPlayerTransform.TransformDirection(m_offSet);
+            transform.eulerAngles = m_trPlayerTransform.eulerAngles + m_trPlayerTransform.TransformDirection(m_offSet);
+        }
     }
 }
