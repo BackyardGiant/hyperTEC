821db62e7b9a4fe5b11e9829b735ee23a0eefa45
Jack Brewer <j.brewer@hotmail.co.uk>
2019-10-06 02:32:29 +0100

    S2C4 - Space Dust
    A simple particle system has been added for space dust, the values are changes based on the player so that the particles can keep up.

---
+++ Codename - Junker (Unity Project)/Assets/Scripts/Management/DustManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private ParticleSystem m_particleSystem;
+    [SerializeField]
+    private float m_playbackScale;
+
+    [SerializeField]
+    private Rigidbody m_rbPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_particleSystem.playbackSpeed = (m_rbPlayer.velocity.magnitude * m_playbackScale) + 1;
+    }
+}
