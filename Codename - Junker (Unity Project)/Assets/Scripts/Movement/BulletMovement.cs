4fd7ca1a833b8ed089215031493e34794f619eb3
Jack Brewer <h[afiwhd>
2019-10-16 16:10:18 +0100

    S3C2 - Shooting
    When you press the trigger a bullet is spawned and it flies away from the player.

---
+++ Codename - Junker (Unity Project)/Assets/Scripts/Movement/BulletMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    private float m_lifeTime;
+
+    public float LifeTime { get => m_lifeTime; set => m_lifeTime = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * 100 * Time.deltaTime;
+    }
+
+    public void Instantiated()
+    {
+        Invoke("DestroySelf", m_lifeTime);
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Codename - Junker (Unity Project)/Assets/Scripts/Shooting/PlayerShooting.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField, Header("Bullet Spawning"), Tooltip("Put objetcs here that the bullet will spawn from, right weapon is position 0 left weapon is position 1")]
+    private GameObject[] m_spawnLocations;
+
+    [SerializeField, Header("Bullets"), Tooltip("Bullet prefab")]
+    private GameObject m_bulletPrefab;
+
+    #region Cooldowns
+    [SerializeField, Header("Cooldown"), Tooltip("The time taken for the right weapon to ready to fire")]
+    private float m_rightWeaponCooldown;
+    [SerializeField,Tooltip("The time taken for the right weapon to ready to fire")]
+    private float m_leftWeaponCooldown;
+    private bool m_rightWeaponActive = true;
+    private bool m_leftWeaponActive = true;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("RightTrigger") > 0.1f && m_rightWeaponActive)
+        {
+            SpawnBullet(0);
+            m_rightWeaponActive = false;
+            StartCoroutine(rightCooldown());
+        }
+
+        if (Input.GetAxis("LeftTrigger") > 0.1f && m_leftWeaponActive)
+        {
+            SpawnBullet(1);
+            m_leftWeaponActive = false;
+            StartCoroutine(leftCooldown());
+        }
+    }
+
+    void SpawnBullet(int _side)
+    {
+        GameObject newBullet = Instantiate(m_bulletPrefab, m_spawnLocations[_side].transform.position, m_spawnLocations[0].transform.rotation);
+        newBullet.GetComponent<BulletMovement>().LifeTime = 2f;
+        newBullet.GetComponent<BulletMovement>().Instantiated();
+    }
+
+    IEnumerator rightCooldown()
+    {
+        yield return new WaitForSeconds(m_rightWeaponCooldown);
+        m_rightWeaponActive = true;
+    }
+
+    IEnumerator leftCooldown()
+    {
+        yield return new WaitForSeconds(m_leftWeaponCooldown);
+        m_leftWeaponActive = true;
+    }
+}
