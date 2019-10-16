6d146c2766b916e7dbf8e7ecf34ea4d3fdf62d07
Aden <adenwebb21@gmail.com>
2019-10-16 17:16:18 +0100

    S3C1 - Starting to Implement Modular
    Made a test scene and a test script to start considering how the modular ship bit will work.

---
+++ Codename - Junker (Unity Project)/Assets/Scripts/Temporary Scripts/TestModularShip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestModularShip : MonoBehaviour
+{
+    public GameObject leftGun, rightGun, engine;
+    public Transform leftSnap, rightSnap, engineSnap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            leftGun.transform.position = leftSnap.position;
+            Vector3 newScale = leftGun.transform.localScale;
+            leftGun.transform.localScale = new Vector3(newScale.x, newScale.y, newScale.z * -1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rightGun.transform.position = rightSnap.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            engine.transform.position = engineSnap.position;
+        }
+
+    }
+}
