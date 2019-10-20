103fde1946b2aa1c87fbfc8b39cb0e55ff0c6a93
Jack Griffiths <jackdotgriffiths@gmail.com>
2019-10-20 23:27:14 +0100

    S3C1 - Basic Loot UI
    Loot UI started, displays designed overlay when a piece of loot appears, animated to show further info when 'scanned'

--- Codename - Junker (Unity Project)/Assets/Scripts/Management/HUDManager.cs
+++ Codename - Junker (Unity Project)/Assets/Scripts/Management/HUDManager.cs
@@ -7,14 +7,13 @@ using UnityEngine.UI;
 public class HUDManager : MonoBehaviour
 {
     private static HUDManager s_instance;
+    public GameObject Player;
+    public Sprite TargetSprite;
+    public GameObject LootDisplay;
+    public Image Scanner;
 
-
-    private Canvas m_HUDcanvas;
-
-    public GameObject player;
-
+    #region EnemyIndicators
     [Header ("Enemy Indicator System")]
-    public Sprite enemyTarget;
     public Sprite enemyArrowPointer;
     [SerializeField, Tooltip("Colour of the enemy indicators")]
     private Color m_enemyTargetColour;
@@ -25,24 +24,41 @@ public class HUDManager : MonoBehaviour
     [SerializeField, Tooltip("Distance at which enemies should be detected")]
     private int m_viewDistance;
     private float m_arrowClampAngle;
+    #endregion
+
+    #region LootIndicators
+    [Header("Loot Indicator System"),SerializeField, Tooltip("Colour of the loot indicators")]
+    private Color m_lootTargetColour;
+    [SerializeField, Tooltip("Scale of the loot indicators")]
+    private float m_lootTargetSize;
+    [SerializeField, Tooltip("Distance at which loot should be detected")]
+    private int m_lootViewDistance;
+    [SerializeField, Tooltip("How far away from the target that the loot display appears"),Range(0f,1f)]
+    private float m_displayOffset = 0.1f;
+    [SerializeField, Tooltip("How quickly the scan occurs. Higher is faster."), Range(0.2f,1f)]
+    private float m_fillSpeed;
+    #endregion
 
 
+    private Canvas m_HUDcanvas;
+    private bool m_displayAnimated;
+    private bool m_currentlyScanning = false;
+    private GameObject m_DisplayDismissed;
     public static HUDManager Instance { get => s_instance; set => s_instance = value; }
+    public int ViewDistance { get => m_viewDistance; }
+    public int LootViewDistance { get => m_lootViewDistance; }
 
-    public int ViewDistance { get => m_viewDistance; set => m_viewDistance = value; }
-
-    private void Start()
+    void Start()
     {
         m_HUDcanvas = GetComponent<Canvas>();
-
-       // m_arrowClampAngle = Mathf.Asin((Screen.height / 2) / Mathf.Sqrt((m_viewDistance * m_viewDistance) + (Screen.height / 2 * Screen.height / 2)));
-
+        m_displayAnimated = false;
         m_arrowClampAngle = Mathf.Asin((Screen.height) / Mathf.Sqrt((m_viewDistance * m_viewDistance) + (Screen.height * Screen.height)));
         m_arrowClampAngle = m_arrowClampAngle * Mathf.Rad2Deg;
-        Debug.Log(m_arrowClampAngle);
     }
-    private void Awake()
+    void Awake()
     {
+        LootDisplay.SetActive(false);
+
         if (s_instance == null)
         {
             s_instance = this;
@@ -53,16 +69,50 @@ public class HUDManager : MonoBehaviour
             s_instance = this;
         }
     }
+    void Update()
+    {
+        if (Input.GetButton("Interact") && LootDisplay.activeInHierarchy == true)
+        {
+            Scanner.fillAmount += m_fillSpeed * Time.deltaTime;
+        }
+        if (Input.GetButtonUp("Interact") && LootDisplay.activeInHierarchy == true)
+        {
+            Scanner.fillAmount = 0;
+        }
+        if (Scanner.fillAmount == 1 && m_currentlyScanning == false)
+        {
+            LootDisplay.GetComponent<Animator>().Play("DisplayStats");
+            m_currentlyScanning = true;
+            Scanner.fillAmount = 0;
+        }
+        if (Scanner.fillAmount == 1 && m_currentlyScanning == true)
+        {
+            LootDisplay.GetComponent<Animator>().Play("HideStats");
+            m_currentlyScanning = false;
+            Scanner.fillAmount = 0;
+        }
+        //if (Input.GetButton("Dismiss") && LootDisplay.activeInHierarchy == true)
+        //{
+        //    Scanner.fillAmount = 0;
+        //    m_currentlyScanning = false;
+        //    LootDisplay.SetActive(false);
+        //}
 
+
+    }
+
+
+
+    #region Enemy Detection Methods
     // The argument enemy passed in is the same as the enemy calling the function so that the targets stay encapsulated
     public void DrawEnemyTarget(Vector2 _screenPos, EnemyDetection _enemy)
     {
         GameObject _target;
         Image _targetImage;
 
-        if (_enemy.Target != null)
+        if (_enemy.EnemyTarget != null)
         {
-            _target = _enemy.Target;
+            _target = _enemy.EnemyTarget;
             _targetImage = _target.GetComponent<Image>();
         }
         else
@@ -74,12 +124,12 @@ public class HUDManager : MonoBehaviour
             //Setting the sprite
             _targetImage = _target.AddComponent<Image>();
             _targetImage.color = m_enemyTargetColour;
-            _enemy.Target = _target;
+            _enemy.EnemyTarget = _target;
         }
 
-        //mooveit
+        //moveit
         _targetImage.rectTransform.localScale = new Vector3(m_enemyTargetSize, m_enemyTargetSize, m_enemyTargetSize);
-        _targetImage.sprite = enemyTarget;
+        _targetImage.sprite =TargetSprite;
         _targetImage.transform.position = _screenPos;
         _targetImage.transform.localEulerAngles = Vector3.zero;
 
@@ -90,9 +140,9 @@ public class HUDManager : MonoBehaviour
         GameObject _target;
         Image _targetImage;
         //Draw arrow towards enemy if they're off screen and within range.
-        if (_enemy.Target != null)
+        if (_enemy.EnemyTarget != null)
         {
-            _target = _enemy.Target;
+            _target = _enemy.EnemyTarget;
             _targetImage = _target.GetComponent<Image>();
         }
         else
@@ -104,7 +154,7 @@ public class HUDManager : MonoBehaviour
             //Setting the sprite
             _targetImage = _target.AddComponent<Image>();
             _targetImage.color = m_enemyTargetColour;
-            _enemy.Target = _target;
+            _enemy.EnemyTarget = _target;
         }
 
 
@@ -131,8 +181,8 @@ public class HUDManager : MonoBehaviour
         _screenPos.x = Mathf.Clamp(_screenPos.x, 20, Screen.width - 20);
 
 
-        Vector3 _enemyToPlayer = _enemy.transform.position - player.transform.position;
-        float _angleBehind = Vector3.Angle(_enemyToPlayer, -player.transform.forward);
+        Vector3 _enemyToPlayer = _enemy.transform.position - Player.transform.position;
+        float _angleBehind = Vector3.Angle(_enemyToPlayer, -Player.transform.forward);
 
 
         if (_angleBehind > m_arrowClampAngle)
@@ -144,9 +194,62 @@ public class HUDManager : MonoBehaviour
             ClearEnemyDetection(_enemy);
         }
     }
-
     public void ClearEnemyDetection(EnemyDetection _enemy)
     {
-        Destroy(_enemy.Target);
+        Destroy(_enemy.EnemyTarget);
     }
+    #endregion
+    #region Loot Detection Methods
+    public void DrawLootTarget(Vector2 _screenPos, LootDetection _loot)
+    {
+        GameObject _lootObject;
+        Image _targetImage;
+
+        if (_loot.LootTarget != null)
+        {
+            _lootObject = _loot.LootTarget;
+            _targetImage = _lootObject.GetComponent<Image>();
+        }
+        else
+        {
+            _lootObject = new GameObject();
+            _lootObject.name = "LootTarget";
+            _lootObject.transform.parent = this.transform;
+
+            //Setting the sprite
+            _targetImage = _lootObject.AddComponent<Image>();
+            _targetImage.color = m_lootTargetColour;
+            _loot.LootTarget = _lootObject;
+        }
+
+        //moveit
+        _targetImage.rectTransform.localScale = new Vector3(m_lootTargetSize, m_lootTargetSize, m_lootTargetSize);
+        _targetImage.sprite = TargetSprite;
+        _targetImage.transform.position = _screenPos;
+        _targetImage.transform.localEulerAngles = Vector3.zero;
+
+        DrawLootDisplay(_screenPos,_loot);
+    }
+    public void ClearLootTarget(LootDetection _loot)
+    {
+        Destroy(_loot.LootTarget);
+        m_displayAnimated = false;
+        Scanner.fillAmount = 0;
+        LootDisplay.SetActive(false);
+    }
+    private void DrawLootDisplay(Vector2 _targetPos, LootDetection _loot)
+    {
+        LootDisplay.SetActive(true);
+        if (!m_displayAnimated)
+        {
+            LootDisplay.GetComponent<Animator>().Play("ShowLoot");
+            m_displayAnimated = true;
+        }
+        Vector3 _displayTargetPos;
+        _displayTargetPos = new Vector3(_targetPos.x, _targetPos.y + m_displayOffset * Screen.height);
+        LootDisplay.GetComponent<RectTransform>().position = _displayTargetPos;
+    }
+
+    #endregion
+
 }
