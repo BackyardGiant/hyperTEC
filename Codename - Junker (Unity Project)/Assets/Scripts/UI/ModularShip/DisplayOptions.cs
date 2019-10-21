62b704acd07adcd76de380aa58d25b068c7aebec
Aden <adenwebb21@gmail.com>
2019-10-21 18:19:46 +0100

    S4C1 - Worked more on modular ship system
    I have added some UI for the modular ship system. The player model spins on the screen and there is a list of modules on the side. A highlight panel can now be used to choose between different modules

---
+++ Codename - Junker (Unity Project)/Assets/Scripts/UI/ModularShip/DisplayOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayOptions : MonoBehaviour
+{
+    public RectTransform contentPanel;
+    public GameObject listItemPrefab;
+
+    public List<GameObject> modulesList;
+
+    private void Awake()
+    {
+        for (int i = 0; i < modulesList.Count; i++)
+        {
+            GameObject newModule = GameObject.Instantiate(modulesList[i]);
+            newModule.transform.SetParent(contentPanel);
+        }
+    }
+}
