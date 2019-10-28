1635c3a79b63f23d8c807086805cd576b79d5f5c
Jack Brewer <jack.brewer@hotmail.co.uk>
2019-10-28 12:37:53 +0000

    S5C3 - Advanced Enemy Movement
    The enemy now has wonder behavior.

--- Codename - Junker (Unity Project)/Assets/Scripts/Movement/AdvancedEnemyMovement.cs
+++ Codename - Junker (Unity Project)/Assets/Scripts/Movement/AdvancedEnemyMovement.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class AdvancedEnemyMovement : MonoBehaviour
 {
-    #region Player
+    #region Target
     private GameObject m_player;
+    private GameObject m_target;
     private Quaternion m_targetRot;
-    private float m_targetX;
-    private float m_targetY;
-    private float m_targetZ;
     #endregion
 
     #region Stats
@@ -25,15 +23,36 @@ public class AdvancedEnemyMovement : MonoBehaviour
     private float m_detectedRange;
     private GameObject m_closestObsticle;
     private Vector3 m_closestObsticleIntersect = new Vector3(0,0,0);
+    [SerializeField, Tooltip("The distance from the player that the wander circle is generated")]
+    private float m_wanderCircleDistance;
+    [SerializeField, Tooltip("The size of the displacement circle")]
+    private float m_circleRadius;
+    [SerializeField, Tooltip("The chance that the ship will chnage direction when wondering"), Range(0,1)]
+    private float m_turnChance;
+    #endregion
+
+    #region Damping
+    [SerializeField, Header("Damping"), Tooltip("The speed that damping is applied")]
+    private float m_dampingSpeed;
+    [SerializeField, Tooltip("The correction force to counter act damping in the posative direction")]
+    private float m_correctionForce;
+    [SerializeField, Tooltip("The of angle at which damping will be applied (1 is same direction)"), Range(0.5f, 1)]
+    private float m_dampingAngleThreshold;
     #endregion
 
     #region
     [Header("Behaviour")]
     [SerializeField, Tooltip("Is the enemy fleeing the player")]
     private bool m_fleeing;
+    [SerializeField]
+    private bool m_attacking;
     [SerializeField, Tooltip("The strength of the enemies avoidence force")]
     private float m_avoidenceForce;
     private Vector3 m_steering;
+    private Vector3 m_wanderDirection;
+    [SerializeField, Tooltip("The distance from the oragin that the enemy will fly before tunring around and seeking back to the centre")]
+    private float m_wanderRange;
+    private bool m_canWander = true;
     #endregion
 
     private Rigidbody m_rb;
@@ -42,12 +61,8 @@ public class AdvancedEnemyMovement : MonoBehaviour
     {
         m_player = GameObject.FindGameObjectWithTag("Player");
         m_rb = GetComponent<Rigidbody>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        m_target = m_player;
+        m_rb.inertiaTensor = new Vector3(0.2f, 0.2f, 0.2f);
     }
 
     // Update is called once per frame
@@ -94,31 +109,51 @@ public class AdvancedEnemyMovement : MonoBehaviour
             m_closestObsticleIntersect = _hit.point;
         }
 
-
-        else
-        {
-            Debug.DrawRay(transform.position + transform.forward * 3, transform.forward * m_detectionRange, Color.white);
-            Debug.DrawRay(transform.position + transform.right * 5, transform.forward * m_detectionRange / 1.2f, Color.white);
-            Debug.DrawRay(transform.position - transform.up * 3, transform.forward * m_detectionRange / 1.2f, Color.white);
-            Debug.DrawRay(transform.position + transform.up * 3, transform.forward * m_detectionRange / 1.2f, Color.white);
-            Debug.DrawRay(transform.position - transform.right * 5, transform.forward * m_detectionRange / 1.2f, Color.white);
-        }
     }
 
     private void FixedUpdate()
     {
-        m_steering = new Vector3(0,0,0);
 
-        m_steering += collisionAvoidance();
-
-        if (m_fleeing)
+        if (Vector3.Distance(transform.position, Vector3.zero) < m_wanderRange)
         {
-            m_steering += FleePlayer();
+            if (m_attacking)
+            {
+                if (m_fleeing)
+                {
+                    m_steering = FleeTarget(m_target.transform);
+                }
+                else
+                {
+                    m_steering = PursueTarget(m_target.transform);
+                }
+            }
+            else
+            {
+                m_steering = Wander();
+            }
         }
         else
         {
-            m_steering += PursuePlayer();
+            if (m_attacking)
+            {
+                if (m_fleeing)
+                {
+                    m_steering = FleeTarget(m_target.transform);
+                }
+                else
+                {
+                    m_steering = PursueTarget(m_target.transform);
+                }
+            }
+            else
+            {
+                m_steering = Seek(Vector3.zero);
+            }
         }
+
+        m_steering += collisionAvoidance();
+
+        ApplyDamping();
 
         if (Vector3.Angle(m_steering.normalized, m_rb.velocity.normalized) > m_flipAngle)
         {
@@ -132,7 +167,8 @@ public class AdvancedEnemyMovement : MonoBehaviour
             m_rb.AddForce((-m_steering) * (m_rb.velocity.magnitude - m_maxSpeed));
         }
 
-        transform.forward = Vector3.Lerp(transform.forward, m_steering, 0.0002f);
+        transform.forward = Vector3.Lerp(transform.forward, m_steering.normalized * m_maxSpeed, 0.0002f);
+        //transform.forward = m_steering;
     }
 
     private Vector3 Seek(Vector3 _target)
@@ -143,9 +179,9 @@ public class AdvancedEnemyMovement : MonoBehaviour
         return _steering;
     }
 
-    private Vector3 PursuePlayer()
+    private Vector3 PursueTarget(Transform _target)
     {
-        Vector3 _estimatedPosition = m_player.transform.position + m_player.GetComponent<Rigidbody>().velocity;
+        Vector3 _estimatedPosition = _target.position + _target.GetComponent<Rigidbody>().velocity;
         Vector3 _steering = Seek(_estimatedPosition);
 
         return _steering;
@@ -159,12 +195,45 @@ public class AdvancedEnemyMovement : MonoBehaviour
         return _steering;
     }
 
-    private Vector3 FleePlayer()
+    private Vector3 FleeTarget(Transform _target)
     {
-        Vector3 _estimatedPosition = m_player.transform.position + m_player.GetComponent<Rigidbody>().velocity;
+        Vector3 _estimatedPosition = _target.position + _target.GetComponent<Rigidbody>().velocity;
         Vector3 _steering = Flee(_estimatedPosition);
 
         return _steering;
+    }
+
+    private Vector3 Wander()
+    {
+        Vector3 _wanderForce = m_steering;
+
+        if (Random.value < m_turnChance && m_canWander)
+        {
+            m_canWander = false;
+            StartCoroutine(ResetWander());
+            Vector3 _circleCentre;
+            if (m_rb.velocity != Vector3.zero)
+            {
+                _circleCentre = m_rb.velocity.normalized;
+            }
+            else
+            {
+                _circleCentre = transform.forward;
+            }
+
+            _circleCentre *= m_wanderCircleDistance;
+
+            Vector2 randomPoint = Random.insideUnitCircle;
+
+            Vector3 displacement = new Vector3(randomPoint.x, randomPoint.y) * m_circleRadius;
+            displacement = Quaternion.LookRotation(m_rb.velocity) * displacement;
+
+            _wanderForce = _circleCentre + displacement;
+            _wanderForce = (_wanderForce.normalized * m_maxSpeed * m_acceleration) - m_rb.velocity;
+            m_wanderDirection = _wanderForce;
+        }
+
+        return _wanderForce;
     }
 
     private Vector3 collisionAvoidance()
@@ -195,5 +264,25 @@ public class AdvancedEnemyMovement : MonoBehaviour
     private void CalculateDetectionRange(float _detectionRange)
     {
         m_detectedRange = _detectionRange;
+    }
+    private void ApplyDamping()
+    {
+        Vector3 _velcoity = m_rb.velocity;
+        Vector3 _targetVector = m_steering.magnitude * transform.forward * GameManager.Instance.GameSpeed;
+        float _dotProduct = Vector3.Dot(_velcoity.normalized, _targetVector.normalized);
+        if (_targetVector.magnitude > 0.1f)
+        {
+            if (_dotProduct < m_dampingAngleThreshold)
+            {
+                m_rb.AddForce(m_dampingSpeed * -_velcoity * GameManager.Instance.GameSpeed); //damping force
+                m_rb.AddForce(m_correctionForce * transform.forward * GameManager.Instance.GameSpeed); //correction force
+            }
+        }
+    }
+
+    private IEnumerator ResetWander()
+    {
+        yield return new WaitForSeconds(3f);
+        m_canWander = true;
     }
 }
