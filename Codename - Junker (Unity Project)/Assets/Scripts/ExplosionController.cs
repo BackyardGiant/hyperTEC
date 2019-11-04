9f6218e0202ab5912ecc88715f82153519531090
Jack Griffiths <jackdotgriffiths@gmail.com>
2019-11-04 12:17:33 +0000

    S6C11 - Prototype Explosion
    Prototype Explosion created as a prefab, to be instantiated when Enemies die

---
+++ Codename - Junker (Unity Project)/Assets/Scripts/ExplosionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject m_Ring;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float _randomX = Random.Range(0f, 360f);
+        float _randomY = Random.Range(0f, 360f);
+        float _randomZ = Random.Range(0f, 360f);
+
+        m_Ring.transform.rotation = new Quaternion(_randomX, _randomY, _randomZ,1);
+    }
+
+}
