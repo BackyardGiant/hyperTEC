a34fe4828c2de0b982151b2ce5fa5aa162c367dc
Aden <adenwebb21@gmail.com>
2019-11-04 19:42:48 +0000

    S6C8 - Guns are Loot Items
    The guns now spawn as loot items that can be picked up and used

--- Codename - Junker (Unity Project)/Assets/Scripts/UI/Enemy/DropWeapons.cs
+++ Codename - Junker (Unity Project)/Assets/Scripts/UI/Enemy/DropWeapons.cs
@@ -6,7 +6,11 @@ public class DropWeapons : MonoBehaviour
 {
     public Transform leftSnap, rightSnap;
 
+    public GameObject lootItemParent;
+
     private WeaponData m_weapon1, m_weapon2;
+
+    private Vector3 m_defaultScale = new Vector3(1, 1, 1);
 
     private void Start()
     {
@@ -26,33 +30,43 @@ public class DropWeapons : MonoBehaviour
         {
             if(Random.Range(0, 1) < 0.5f)
             {
+                GameObject _lootItem = Instantiate(lootItemParent, leftSnap.position, leftSnap.rotation);
                 GameObject _temp = ModuleManager.Instance.GenerateWeapon(m_weapon1);
-                _temp.transform.position = leftSnap.position;
-                _temp.transform.rotation = leftSnap.rotation;
-                _temp.transform.localScale = new Vector3(2, 2, 2);
+                _temp.transform.SetParent(_lootItem.transform);
+
+                _temp.transform.localPosition = Vector3.zero;
+                _temp.transform.localRotation = Quaternion.identity;
+
+                _temp.transform.localScale = m_defaultScale;
             }
             else
             {
+                GameObject _lootItem = Instantiate(lootItemParent, rightSnap.position, rightSnap.rotation);
                 GameObject _temp = ModuleManager.Instance.GenerateWeapon(m_weapon2);
-                _temp.transform.position = rightSnap.position;
-                _temp.transform.rotation = rightSnap.rotation;
-                _temp.transform.localScale = new Vector3(2, 2, 2);
+
+                _temp.transform.localPosition = Vector3.zero;
+                _temp.transform.localRotation = Quaternion.identity;
+
+                _temp.transform.localScale = m_defaultScale;
             }
 
             Debug.Log(_randomFloat + "Dropped One");
         }
         else
         {
-
+            GameObject _lootItem = Instantiate(lootItemParent, leftSnap.position, leftSnap.rotation);
             GameObject _temp = ModuleManager.Instance.GenerateWeapon(m_weapon1);
-            _temp.transform.position = leftSnap.position;
-            _temp.transform.rotation = leftSnap.rotation;
-            _temp.transform.localScale = new Vector3(2, 2, 2);
+            _temp.transform.localPosition = Vector3.zero;
+            _temp.transform.localRotation = Quaternion.identity;
 
+            _temp.transform.localScale = m_defaultScale;
+
+            _lootItem = Instantiate(lootItemParent, rightSnap.position, rightSnap.rotation);
             _temp = ModuleManager.Instance.GenerateWeapon(m_weapon2);
-            _temp.transform.position = rightSnap.position;
-            _temp.transform.rotation = rightSnap.rotation;
-            _temp.transform.localScale = new Vector3(2, 2, 2);
+
+            _temp.transform.localPosition = Vector3.zero;
+            _temp.transform.localRotation = Quaternion.identity;
+            _temp.transform.localScale = m_defaultScale;
 
             Debug.Log(_randomFloat + "Dropped Two");
         }
