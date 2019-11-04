4e28aa66e670124ff7061dec0d35d6c87f717517
Aden <adenwebb21@gmail.com>
2019-11-04 11:29:55 +0000

    S6C2 - Player Start with Guns
    The player will now start with 2 equipped guns for the testing session, they are both default type

--- Codename - Junker (Unity Project)/Assets/Scripts/SpawnEngine.cs
+++ Codename - Junker (Unity Project)/Assets/Scripts/SpawnEngine.cs
@@ -6,7 +6,7 @@ public class SpawnEngine : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject SnapPoint, DefaultEngine, TraderEngine, ExplorerEngine, ConstructionEngine;
+    private GameObject SnapPoint, DefaultEngine, TraderEngine, ExplorerEngine, ConstructionEngine, RightSnap, LeftSnap;
 
     // Start is called before the first frame update
     void Start()
@@ -15,24 +15,29 @@ public class SpawnEngine : MonoBehaviour
         int _engineChoice = PlayerPrefs.GetInt("ChosenEngine");
         if(_engineChoice == 1)
         {
-            AttatchEngine(DefaultEngine);
+            AttachEngine(DefaultEngine);
         }
         if (_engineChoice == 2)
         {
-            AttatchEngine(TraderEngine);
+            AttachEngine(TraderEngine);
         }
         if (_engineChoice == 3)
         {
-            AttatchEngine(ExplorerEngine);
+            AttachEngine(ExplorerEngine);
         }
         if (_engineChoice == 4)
         {
-            AttatchEngine(ConstructionEngine);
+            AttachEngine(ConstructionEngine);
         }
+
+        #region GenerateDefaultGuns
+        RightSnap.transform.GetChild(0).GetComponent<WeaponGenerator>().GenerateGun();
+        LeftSnap.transform.GetChild(0).GetComponent<WeaponGenerator>().GenerateGun();
+        #endregion
     }
 
     // Update is called once per frame
-    void AttatchEngine(GameObject _engine)
+    void AttachEngine(GameObject _engine)
     {
         try
         {
