d5b640bda6eb650270df663b9b0845049614626c
Jack Brewer <j.brewer@hotmail.co.uk>
2019-11-17 15:52:19 +0000

    S8C4 - Behaviour tree
    Began adding in behaviour states

--- Codename - Junker (Unity Project)/Assets/Scripts/Management/EnemyManager.cs
+++ Codename - Junker (Unity Project)/Assets/Scripts/Management/EnemyManager.cs
@@ -6,14 +6,24 @@ public class EnemyManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject m_target;
+    [SerializeField]
+    private GameObject m_blank;
     private GameObject m_player;
 
     [SerializeField]
     private bool m_attackingPlayer;
 
+    [SerializeField, Tooltip("A random point around the player is generated this gives the outer limit of that point")]
+    private float m_displacementRadius;
+
     public GameObject Target { get => m_target; set => m_target = value; }
     public GameObject Player { get => m_player; set => m_player = value; }
     public bool AttackingPlayer { get => m_attackingPlayer; set => m_attackingPlayer = value; }
+    
+    #region Behaviour States
+    private enum States { Wander, Pursue, Flee, Evade }
+    #endregion
+
 
     private void Awake()
     {
@@ -31,12 +41,19 @@ public class EnemyManager : MonoBehaviour
     {
         if (m_attackingPlayer)
         {
-            m_target = m_player;
+            m_target = GenerateTargetDisplacement(m_player);
         }
         else
         {
             m_target = null;
         }
         
+    }
+
+    GameObject GenerateTargetDisplacement(GameObject _target)
+    {
+        GameObject displacedObject = Instantiate(m_blank, _target.transform);
+        displacedObject.transform.position += Random.insideUnitSphere * m_displacementRadius;
+        return displacedObject;
     }
 }
