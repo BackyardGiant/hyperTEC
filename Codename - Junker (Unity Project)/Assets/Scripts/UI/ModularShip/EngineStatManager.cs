7f890956ee4eb6b5e0d5850be7497026af07302b
Aden Webb <adenwebb21@gmail.com>
2019-11-30 18:08:11 +0000

    S10C4 - Added Module Thumbnails
    All engines and weapons now have an appropriate thumbnail

--- Codename - Junker (Unity Project)/Assets/Scripts/UI/ModularShip/EngineStatManager.cs
+++ Codename - Junker (Unity Project)/Assets/Scripts/UI/ModularShip/EngineStatManager.cs
@@ -10,12 +10,30 @@ public class EngineStatManager : MonoBehaviour
 
     [SerializeField]
     private EngineData data;
+    [SerializeField]
+    private Sprite m_constructionEngine, m_defaultEngine, m_traderEngine, m_explorerEngine;
     public EngineData Data { get => data; set => data = value; }
 
     public void PopulateData()
     {
         name.text = data.name;
-        thumbnail.sprite = data.Thumbnail;
+
+        switch(data.CurrentFaction)
+        {
+            case EngineData.faction.construction:
+                thumbnail.sprite = m_constructionEngine;
+                break;
+            case EngineData.faction.explorer:
+                thumbnail.sprite = m_explorerEngine;
+                break;
+            case EngineData.faction.initial:
+                thumbnail.sprite = m_defaultEngine;
+                break;
+            case EngineData.faction.trader:
+                thumbnail.sprite = m_traderEngine;
+                break;
+        }
+        
 
         if (thumbnail.sprite == null)
         {
