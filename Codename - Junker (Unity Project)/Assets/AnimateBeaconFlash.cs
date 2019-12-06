5d8ba380576c360d26d34a711225d67ce981f9b0
Jack Griffiths <jackdotgriffiths@gmail.com>
2019-12-06 12:22:44 +0000

    S11C4 - Quest Beacon
    Quest Beacon object imported and animated. This will need to be automatically generated from the save in the near future

---
+++ Codename - Junker (Unity Project)/Assets/AnimateBeaconFlash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateBeaconFlash : MonoBehaviour
+{
+//    [SerializeField] private GameObject m_questObject;
+//    [SerializeField] private Animator m_animator;
+//    // Start is called before the first frame update
+//    void Update()
+//    {
+//        if(m_questObject == null && GameManager.Instance.CanLeaveScene == true)
+//        {
+//            m_animator.enabled = false;
+//            this.enabled = false;
+//        }
+//    }
+}
